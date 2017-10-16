91f4c2f1b8d6487cae426710fea023a584a242d0
NestorMorataya <nestor_morataya@outlook.com>
2017-10-15 21:42:04 -0600

    Primer Sprint

---
+++ src/UserBundle/Form/UsuarioType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsuarioType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('username')->add('firstName')->add('lastName')->add('email')->add('password', 'password')->add('isActive','checkbox')->add('createdAt')->add('updateAt')->add('role', 'choice', array('choices' => array('ROLE_ADMIN' => 'Administrator'), 'placeholder' => 'Seleccionar un rol'));
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UserBundle\Entity\Usuario'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'userbundle_usuario';
+    }
+
+
+}
